d565a99622e835d663af7114aac92341363e986c
Rishika Gupta <142630165+chonkycato@users.noreply.github.com>
2024-02-07 13:54:09 +0530

    Third Commit

--- app/src/main/java/com/example/freshyzo/login/LoginFragment.kt
+++ app/src/main/java/com/example/freshyzo/login/LoginFragment.kt
@@ -1,9 +1,13 @@
 package com.example.freshyzo.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.freshyzo.R
 
@@ -21,6 +25,20 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        val mPhoneInput = view.findViewById<EditText>(R.id.phoneInput)
+        val mOtpInput = view.findViewById<EditText>(R.id.otpInput)
+        val mLoginButton = view.findViewById<Button>(R.id.loginButton)
+        val mGetOtp = view.findViewById<TextView>(R.id.getOTP)
+        val mSignUpInstead = view.findViewById<TextView>(R.id.signUpInstead)
+
+        var mPhoneNumber = mPhoneInput.text.toString()
+        var mOtpEntered = mOtpInput.text.toString()
+
+        Log.d("Phone Number:", mPhoneNumber)
+        Log.d("OTP:", mOtpEntered)
+
+
         return view
     }
 
